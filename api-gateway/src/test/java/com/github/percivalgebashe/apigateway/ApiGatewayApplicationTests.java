c36fb0ce28523fecf5d3d6053362433efaf32819
Percival Gebashe <percivalgebashe@gmail.com>
2025-03-12 09:38:45 +0200

    Setting up micro services for api gateway, dicovery and config

---
+++ api-gateway/src/test/java/com/github/percivalgebashe/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.percivalgebashe.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
