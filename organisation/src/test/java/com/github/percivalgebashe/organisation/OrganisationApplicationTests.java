09daad50e6f69faa4248421f35302e8545087d7e
Percival Gebashe <percivalgebashe@gmail.com>
2025-03-12 11:13:39 +0200

    Created bare organisation module

---
+++ organisation/src/test/java/com/github/percivalgebashe/organisation/OrganisationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.percivalgebashe.organisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganisationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
